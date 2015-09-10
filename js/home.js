099c9608e7ce5267baf6353eab0a898413ae35c0
Guest User <Guest@c-02-05-lab.mddn.vuw.ac.nz>
2015-09-11 10:27:02 +1200

    add Project 2

---
+++ js/home.js
@@ -0,0 +1,12 @@
+<script type="text/javascript" src="../js/jssor.slider.mini.js"></script>
+<script>
+    jQuery(document).ready(function ($) {
+        var options = {
+            ..
+        };
+            
+        $("#slider1_container").css("display", "block");
+        var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+        ...
+    });
+</script>
